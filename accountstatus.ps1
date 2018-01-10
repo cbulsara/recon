2e1ecbac457429ecf8af63de7bfdd8c99c4668b5
cbulsara <bulsaracyrus@gmail.com>
2018-01-10 10:35:03 -0800

    Uploaded powershell scripts for looking up AD users by name, status

---
+++ accountstatus.ps1
@@ -0,0 +1,39 @@
+﻿## Lookup account status based on employee ID # (SamAccountName).
+## Infile = a CSV file with a column named "SamAccountName" that contains...SAM Account Names.
+##          Doesn't like middle initials.
+## Outfile = path to an output CSV that will contain the AD info.
+
+Param (
+    [Parameter(mandatory = $true)]
+    [String]$infile,
+    [Parameter(mandatory = $true)]
+    [String]$outfile
+)
+echo "Infile = $infile"
+echo "Outfile = $outfile"
+$userlist = import-csv $infile
+
+echo "[*]     By ID"
+foreach($user in $userlist) {
+    $id = $user.SamAccountName
+    if ($id) {
+        Get-ADUser $id -Properties Name,SamAccountName,Department,Title,Enabled | Select-Object -Property Name,SamAccountName,Department,Title,Enabled | Export-Csv -Path $outfile -notype -append
+    }
+}
+
+#echo "[*]     ID *"
+#foreach($user in $userlist) {
+#    $id = $user.employee_id
+#    if ($id) {
+#        Get-ADUser -Filter "SamAccountName -Like '*$id*'" | Select-Object -Property Name,Enabled
+#    }
+#}
+
+#echo "[*]    By Name"
+#foreach($user in $userlist) {
+#    $name = $user.name
+#    if ($name) {
+#        Get-ADUser -Filter {Name -Like $name} -Properties Name,Enabled
+#    }
+#}
+
---
+++ namelookup.ps1
@@ -0,0 +1,22 @@
+﻿## Lookup relevant account information based on username.
+## Infile = a CSV file with a column named "name" that contains usernames in Last, First format.
+##          Doesn't like middle initials.
+## Outfile = path to an output CSV that will contain the AD info.
+
+Param (
+    [Parameter(mandatory = $true)]
+    [String]$infile,
+    [Parameter(mandatory = $true)]
+    [String]$outfile
+)
+echo "Infile = $infile"
+echo "Outfile = $outfile"
+$userlist = import-csv $infile
+
+echo "[*]     Lookup Employee ID# Based on *Name*"
+foreach($user in $userlist) {
+    $name = $user.name
+    if ($name) {
+        Get-ADUser -Filter "Name -Like '*$name*'" -Properties Name,SamAccountName,Department,Title | Select-Object -Property Name,SamAccountName,Department,Title | Export-Csv -Path $outfile -notype -append
+    }
+}
